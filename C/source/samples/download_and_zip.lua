d458c13f90fb012662c2f6932599796c3dae1c89
Rinnegatamante <rinnegatamante@gmail.com>
2015-03-25 18:43:00 +0100

    Deleted lpp-3ds fork. [See main repository]
    Deleted lpp-3ds fork. [See main repository]

--- C/source/samples/download_and_zip.lua
+++
@@ -1,6 +0,0 @@
-if Network.isWifiEnabled() then
-	Network.downloadFile("http://rinnegatamante.netsons.org/ORG/0.7/TEST.zip","/Downloaded.zip")
-	System.extractZIP("/Downloaded.zip",System.currentDirectory().."/")
-	System.deleteFile("/Downloaded.zip")
-end
-System.exit()
--- C/source/samples/helloworld.lua
+++
@@ -1,6 +0,0 @@
-while true do
-Screen.waitVblankStart()
-Screen.refresh()
-Screen.debugPrint(0,0,"Hello World",0xFFFFFF,TOP_SCREEN)
-Screen.flip()
-end
