2df48ad218f27f54d1b557ac1d17fe70257ffc04
Rinnegatamante <rinnegatamante@gmail.com>
2016-02-07 11:59:13 +0100

    Added Danzeff keyboard support.
    Added Danzeff keyboard support.

--- LUA/modules/mail.lua
+++ LUA/modules/mail.lua
@@ -3,18 +3,16 @@ mode = "Mail"
 
 -- Internal module settings
 if Network.isWifiEnabled() then
-	if build == "Ninjhax 2" then
-		ShowError("Network features currently unavailable on Ninjhax 2.")
-		CallMainMenu()
-	else
-		object = "Mail Object"
-		to = "sample@gmail.com"
-		body = "This is my e-mail body."
-	end
+	object = "Mail Object"
+	to = "sample@gmail.com"
+	body = "This is my e-mail body."
 else
 	ShowError("You need to be connected on Internet to send mails.")
 	CallMainMenu()
 end
+CallKeyboard(70,20)
+state = "Main"
+screenshots = false
 
 -- Rendering functions
 function AppTopScreenRender()	
@@ -23,6 +21,9 @@ function AppTopScreenRender()
 end
 
 function AppBottomScreenRender()
+	ShowKeyboard()
+	Graphics.fillRect(5,315,190,230,black)
+	Graphics.fillRect(6,314,191,229,white)
 end
 
 -- Module main cycle
@@ -32,29 +33,68 @@ function AppMainCycle()
 	TopCropPrint(9,45,"Obj: "..object,black,TOP_SCREEN)
 	TopCropPrint(9,60,"To: "..to,black,TOP_SCREEN)
 	Font.print(ttf,9,75,"Body:",black,TOP_SCREEN)
+	CropPrint(9,195,"State: "..state,black,BOTTOM_SCREEN)
 	text = LinesGenerator(body,90)
 	for i,line in pairs(text) do
 		Font.print(ttf,9,line[2],line[1],black,TOP_SCREEN)
 	end
 	
-	-- Sets controls triggering
-	if Controls.check(pad,KEY_A) and not Controls.check(oldpad,KEY_A) then
-		body = System.startKeyboard(body)
-	elseif Controls.check(pad,KEY_X) then
-		to = System.startKeyboard(to)
-	elseif Controls.check(pad,KEY_Y) then
-		object = System.startKeyboard(object)
-	elseif Controls.check(pad,KEY_R) and not Controls.check(oldpad,KEY_R) then
-		if Network.isWifiEnabled() then
-			if Network.sendMail(to,object,body) then
-				ShowWarning("Mail sent successfully!")
-			else
-				ShowError("An error has occurred while sending mail.")
-			end	
-		else
-			ShowError("You need to be connected on Internet to send mails.")
+	-- Keyboard input
+	if state ~= "Main" then
+		in_game = true -- Disable START button function
+		CropPrint(9,213,"Press L to confirm",black,BOTTOM_SCREEN)
+		if Controls.check(pad,KEY_L) and not Controls.check(oldpad,KEY_L) then
+			state = "Main"
 		end
-	elseif Controls.check(pad,KEY_B) or Controls.check(pad,KEY_START) then
-		CallMainMenu()
+		input = KeyboardInput()
+		if input > 0 then
+			if state == "Body" then
+				tmp = body
+			elseif state == "To" then
+				tmp = to
+			else
+				tmp = object
+			end
+			if input == 0x09 then
+				if string.len(tmp) > 1 then
+					tmp = string.sub(tmp,1,string.len(tmp)-1)
+				else
+					tmp = ""
+				end
+			else
+				tmp = tmp .. string.char(input)
+			end
+			if state == "Body" then
+				body = tmp
+			elseif state == "To" then
+				to = tmp
+			else
+				object = tmp
+			end
+		end
+	end
+	
+	-- Sets controls triggering
+	if state == "Main" then
+		if Controls.check(pad,KEY_A) and not Controls.check(oldpad,KEY_A) then
+			state = "Body"
+		elseif Controls.check(pad,KEY_X) then
+			state = "To"
+		elseif Controls.check(pad,KEY_Y) then
+			state = "Obj"
+		elseif Controls.check(pad,KEY_R) and not Controls.check(oldpad,KEY_R) then
+			if Network.isWifiEnabled() then
+				if Network.sendMail(to,object,body) then
+					ShowWarning("Mail sent successfully!")
+				else
+					ShowError("An error has occurred while sending mail.")
+				end	
+			else
+				ShowError("You need to be connected on Internet to send mails.")
+			end
+		elseif Controls.check(pad,KEY_B) or Controls.check(pad,KEY_START) then
+			CloseKeyboard()
+			CallMainMenu()
+		end
 	end
 end
--- LUA/scripts/funcs.lua
+++ LUA/scripts/funcs.lua
@@ -28,6 +28,7 @@ function CallMainMenu()
 	renderer = true
 	screenshots = true
 	refresh_screen = true
+	refresh_screen2 = true
 	System.currentDirectory(start_dir)
 end
 
@@ -305,4 +306,115 @@ function OneshotPrint(my_func)
 	Screen.flip()
 	Screen.refresh()
 	my_func()
+end
+
+-- * CallKeyboard
+-- Calls a new instance for a Danzeff Keyboard
+function CallKeyboard(x, y)
+	danzeff_mode = 1
+	blockx = 2
+	blocky = 2
+	danzeff_x = x
+	danzeff_y = y
+	danzeff_map = {
+		",abc.def!ghi-jkl\009m n?opq(rst:uvw)xyz",
+		"\0\0\0001\0\0\0002\0\0\0003\0\0\0004\009\0 5\0\0\0006\0\0\0007\0\0\0008\0\00009",
+		"^ABC@DEF*GHI_JKL\009M N\"OPQ=RST;UVW/XYZ",
+		"'(.)\"<'>-[_]!{?}\009\0 \0+\\=/:@;#~$`%*^|&"
+	}
+	pic1 = Graphics.loadImage(theme_dir.."/images/keys.png")
+	pic2 = Graphics.loadImage(theme_dir.."/images/nums.png")
+	pic3 = Graphics.loadImage(theme_dir.."/images/keys_c.png")
+	pic4 = Graphics.loadImage(theme_dir.."/images/nums_c.png")
+	pic1m = Graphics.loadImage(theme_dir.."/images/keys_t.png")
+	pic2m = Graphics.loadImage(theme_dir.."/images/nums_t.png")
+	pic3m = Graphics.loadImage(theme_dir.."/images/keys_c_t.png")
+	pic4m = Graphics.loadImage(theme_dir.."/images/nums_c_t.png")
+	olddanzpad = KEY_A
+end
+
+-- * ShowKeyboard
+-- Shows current keyboard instance
+function ShowKeyboard()
+	if danzeff_mode == 1 then
+		h = pic1
+		Graphics.drawImage(danzeff_x, danzeff_y, pic1m)
+	elseif danzeff_mode == 2 then
+		h = pic2
+		Graphics.drawImage(danzeff_x, danzeff_y, pic2m)
+	elseif danzeff_mode == 3 then
+		h = pic3
+		Graphics.drawImage(danzeff_x, danzeff_y, pic3m)
+	else
+		h = pic4
+		Graphics.drawImage(danzeff_x, danzeff_y, pic4m)
+	end
+	danx = (blockx - 1) * 50
+	dany = (blocky - 1) * 50
+	Graphics.drawPartialImage(danzeff_x + danx, danzeff_y + dany, danx, dany, 50, 50, h)
+end
+
+-- * KeyboardInput()
+-- Gets current keyboard input
+function KeyboardInput()		
+	cx, cy = Controls.readCirclePad()
+	danzpad = Controls.read()
+	posx = 2
+	posy = 2	
+	if cx < -50 then
+		posx = 1
+	end
+	if cx > 50 then
+		posx = 3
+	end
+	if cy > 50 then
+		posy = 1
+	end
+	if cy < -50 then
+		posy = 3
+	end	
+	if blocky ~= posy or blockx ~= posx then
+		blocky = posy
+		blockx = posx
+	end	
+	if danzeff_mode > 2 then
+		danzeff_mode = danzeff_mode - 2
+	end	
+	if Controls.check(danzpad, KEY_R) and not Controls.check(olddanzpad, KEY_R) then
+		if danzeff_mode == 1 then
+			danzeff_mode = 2
+		else
+			danzeff_mode = 1
+		end
+	end
+	if Controls.check(danzpad, KEY_R) then
+		danzeff_mode = danzeff_mode + 2
+	end	
+	charpos = (blocky - 1) * 12 + (blockx - 1) * 4
+	if Controls.check(danzpad, KEY_Y) and not Controls.check(olddanzpad, KEY_Y) then
+		res = string.byte(danzeff_map[danzeff_mode], charpos + 2)
+	elseif Controls.check(danzpad, KEY_A) and not Controls.check(olddanzpad, KEY_A) then
+		res = string.byte(danzeff_map[danzeff_mode], charpos + 4)
+	elseif Controls.check(danzpad, KEY_B) and not Controls.check(olddanzpad, KEY_B) then
+		res = string.byte(danzeff_map[danzeff_mode], charpos + 3)
+	elseif Controls.check(danzpad, KEY_X) and not Controls.check(olddanzpad, KEY_X) then
+		res = string.byte(danzeff_map[danzeff_mode], charpos + 1)
+	else
+		res = 0
+	end	
+	olddanzpad = danzpad
+	return res
+end
+
+-- * CloseKeyboard()
+-- Close current keyboard instance
+function CloseKeyboard()
+	Graphics.freeImage(pic1)
+	Graphics.freeImage(pic2)
+	Graphics.freeImage(pic3)
+	Graphics.freeImage(pic4)
+	Graphics.freeImage(pic1m)
+	Graphics.freeImage(pic2m)
+	Graphics.freeImage(pic3m)
+	Graphics.freeImage(pic4m)
 end
